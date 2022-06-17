38610bb417bdf237a1f3c92f7226322e3f7c1570
Pavel Volokitin <pavelvolokitin@gmail.com>
2022-06-17 22:37:59 +0700

    0.4.1

--- Formula/mprocs.rb
+++ Formula/mprocs.rb
@@ -1,9 +1,9 @@
 class Mprocs < Formula
   desc "Run multiple commands in parallel"
   homepage "https://github.com/pvolok/mprocs"
-  url "https://github.com/pvolok/mprocs/releases/download/v0.4.0/mprocs-0.4.0-macos64.tar.gz"
-  sha256 "a490df8982a79460f72cd3e0ebc55b115872f89413016fffe9a2851434089638"
-  version "0.4.0"
+  url "https://github.com/pvolok/mprocs/releases/download/v0.4.1/mprocs-0.4.1-macos64.tar.gz"
+  sha256 "9c23038dfdf6f4058fb9a41d3c6c815a75585d70fa7e785d152b10456a71e4da"
+  version "0.4.1"
 
   def install
     bin.install "mprocs"
